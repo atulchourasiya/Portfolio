216531ea20b85208a65bc4d1e91723dbd398be24
Atul chourasiya <82812953+atulchourasiya@users.noreply.github.com>
2024-01-28 13:14:08 +0530

    Added Hashrouter
    Signed-off-by: Atul chourasiya <82812953+atulchourasiya@users.noreply.github.com>

--- src/Component/Overlay/AppRoutes.jsx
+++ src/Component/Overlay/AppRoutes.jsx
@@ -1,5 +1,5 @@
 import React, { useContext } from 'react';
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { HashRouter, Route, Routes } from 'react-router-dom';
 import Project from './Project/Project';
 import Contact from './Contact/Contact';
 import Navbar from './Navbar/Navbar';
@@ -9,15 +9,15 @@ import LoadingScreen from './LoadingScreen/LoadingScreen';
 const AppRoutes = () => {
    const { loading } = useContext(LoadingContext);
    return (
-      <BrowserRouter>
-         <Routes>
-            <Route path='/project' exact element={<Project />} />
-            <Route path='/contact' exact element={<Contact />} />
-         </Routes>
-         {loading && <LoadingScreen />}
-         <Navbar />
-      </BrowserRouter>
+      loading ? <LoadingScreen /> :
+         <HashRouter>
+            <Routes>
+               <Route path='/project' exact element={<Project />} />
+               <Route path='/contact' exact element={<Contact />} />
+            </Routes>
+            <Navbar />
+         </HashRouter>
    );
 };
 
-export default AppRoutes; 
+export default AppRoutes;
