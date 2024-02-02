b28eebe3a887db6ec23a8a5bd9d7d657de1b370f
Atul chourasiya <82812953+atulchourasiya@users.noreply.github.com>
2024-02-02 22:22:37 +0530

    fixed planet position
    Signed-off-by: Atul chourasiya <82812953+atulchourasiya@users.noreply.github.com>

--- src/Component/Canvas/Planets.jsx
+++ src/Component/Canvas/Planets.jsx
@@ -1,6 +1,5 @@
 import { useRef } from 'react';
 import { Decal, Float, useTexture } from '@react-three/drei';
-import { planetArray } from './PlanetData';
 import { usePlanetPosition } from '../Utility/usePlanetPosition';
 import { useThree } from 'react-three-fiber';
 
@@ -8,12 +7,35 @@ const Planets = () => {
    const { camera, viewport } = useThree();
    const { width, height } = viewport;
 
+   const planetArray = [
+      {
+         decalTexture: '/reactTexture.png',
+         planetTexture: '/reactPlanet.jpg',
+         position: [-(width/2) + 6, 20, -40]
+      },
+      {
+         decalTexture: '/javascriptTexture.png',
+         planetTexture: '/javascriptPlanet.jpg',
+         position: [-((width / 2)/1.5) + 6, 5, -40]
+      },
+      {
+         decalTexture: '/nodeTexture.png',
+         planetTexture: '/nodePlanet.jpg',
+         position: [((width / 2) ) -6, 20, -40]
+      },
+      {
+         decalTexture: '/reduxTexture.png',
+         planetTexture: '/reduxPlanet.jpg',
+         position : [(width / 2) - 6, 2, -40]
+      },
+   ];
+
    return planetArray.map((planet, index) => {
       return <Planet
          key={index}
          decalTexture={planet.decalTexture}
          planetTexture={planet.planetTexture}
-         position={usePlanetPosition(width, height, index)}
+         position={planet.position || usePlanetPosition(width, height, index)}
       />;
    });
 
@@ -27,7 +49,7 @@ const Planet = ({ decalTexture, planetTexture, position }) => {
    const decalRef = useRef();
    return (
       <Float speed={1.75} rotationIntensity={1.5} floatIntensity={2.5} position={position}>
-         <mesh ref={planetRef} castShadow receiveShadow scale={6}>
+         <mesh ref={planetRef} castShadow receiveShadow scale={window.innerWidth > 768 ? 5 : 3}>
             <sphereGeometry args={[1, 20, 20]} />
             <meshStandardMaterial
                map={planet}
@@ -35,7 +57,7 @@ const Planet = ({ decalTexture, planetTexture, position }) => {
                polygonOffsetFactor={-5}
                flatShading
                color={
-                  [1.5, 1.5, 1.5]
+                  [1.1, 1.1, 1.1]
                } 
                toneMapped={false}
             />
