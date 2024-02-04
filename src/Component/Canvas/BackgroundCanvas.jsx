ae9a16878887431078aed4dc84d006211b943bac
Atul chourasiya <82812953+atulchourasiya@users.noreply.github.com>
2024-02-04 17:21:35 +0530

    Fixed CameraRig
    Signed-off-by: Atul chourasiya <82812953+atulchourasiya@users.noreply.github.com>

--- src/Component/Canvas/BackgroundCanvas.jsx
+++ src/Component/Canvas/BackgroundCanvas.jsx
@@ -4,11 +4,11 @@ import ComputerModel from './ComputerModel';
 import ShootingStar from './ShootingStar';
 import { EffectComposer, Bloom } from '@react-three/postprocessing';
 import BackgroundText from './BackgroundText';
-import { useRef, useEffect, Suspense } from 'react';
+import { useRef, useEffect, Suspense, useState } from 'react';
 import { easing } from 'maath';
-import { useThree } from 'react-three-fiber';
 import Planets from './Planets';
 import { Perf } from 'r3f-perf';
+
 export default function BackgroundCanvas() {
    return (
       <Canvas className='!h-dvh' shadows dpr={[1, 1.5]} camera={{ position: [-1.5, 1, 5.5], fov: 45, near: 80, far: 100 }} eventSource={document.getElementById('root')} eventPrefix="client">
@@ -24,23 +24,33 @@ export default function BackgroundCanvas() {
          <fog attach="fog" args={['black', 140, 170]} />
          {/* <Perf openByDefault trackGPU={true} position={'top-right'} /> */}
          <Suspense fallback={null}>
-            {/* <CameraRig > */}
-               <Stars />
-               <ShootingStar />
-               <ComputerModel />
-               <BackgroundText />
-               <Planets />
-            {/* </CameraRig> */}
+            {
+               window.matchMedia('(pointer: fine)').matches ?
+                  <CameraRig >
+                     <Stars />
+                     <ShootingStar />
+                     <ComputerModel />
+                     <BackgroundText />
+                     <Planets />
+                  </CameraRig> :
+                  <>
+                     <Stars />
+                     <ShootingStar />
+                     <ComputerModel />
+                     <BackgroundText />
+                     <Planets />
+                  </>
+            }
          </Suspense>
          <OrbitControls
             enableZoom={false}
             enablePan={false}
             maxPolarAngle={Math.PI / 2 - 0.3}
             minPolarAngle={Math.PI / 2 - 0.3}
-            minAzimuthAngle={window.innerWidth > 768 ? (-Math.PI / 4) : (-Math.PI / 8)}  // Limit to 45 degrees to the left
-            maxAzimuthAngle={window.innerWidth > 768 ? Math.PI / 4 : Math.PI / 8 } // Limit to 45 degrees to the right
+            minAzimuthAngle={window.matchMedia('(pointer : fine)').matches ? 0 : (-Math.PI / 8)}  // Limit to 45 degrees to the left
+            maxAzimuthAngle={window.matchMedia('(pointer : fine)').matches ? 0 : (Math.PI / 8)} // Limit to 45 degrees to the right
             enableDamping={true}
-            // enableRotate={false}
+            enableRotate={window.matchMedia('(pointer: fine)').matches ? false : true}
          />
          <Preload all />
          <BakeShadows />
@@ -52,51 +62,38 @@ export default function BackgroundCanvas() {
 }
 
 
-// function CameraRig({ children }) {
-//    const groupRef = useRef();
-//    const pointer = useRef({ x: 0, y: 0 });
-
-
-//    useEffect(() => {
-//       document.addEventListener('mousemove', handleMouseMove);
-//       document.addEventListener('mouseout', handleMouseOut);
-//       return () => {
-//          document.removeEventListener('mousemove', handleMouseMove);
-//          document.removeEventListener('mouseout', handleMouseOut);
-//       };
-//    }, []);
-
-//    const handleMouseMove = (event) => {
-//       const canvas = document.querySelector('canvas');
-//       const canvasRect = canvas.getBoundingClientRect();
-
-//       const mouseX = event.clientX - canvasRect.left;
-//       const mouseY = event.clientY - canvasRect.top;
-
-//       pointer.current = {
-//          x: (mouseX / canvasRect.width) * 2 - 1,
-//          y: -(mouseY / canvasRect.height) * 2 + 1,
-//       };
-//    };
-
-//    const handleMouseOut = () => {
-//       pointer.current.x = 0;
-//       pointer.current.y = 0;
-//    };
-
-//    useFrame((state, delta) => {
-//       easing.dampE(
-//          groupRef.current.rotation,
-//          [0, pointer.current.x / 4, 0],
-//          0.35,
-//          delta
-//       );
-//       if (window.matchMedia('(pointer: coarse)').matches && pointer.current.x > 0) {
-//          pointer.current.x -= .05;
-//       } else if (window.matchMedia('(pointer: coarse)').matches && pointer.current.x < 0) {
-//          pointer.current.x += .05;
-//       }
-//    });
-
-//    return <group ref={groupRef}>{children}</group>;
-// }
+function CameraRig({ children }) {
+   const [mouseOut, setMouseOut] = useState(false);
+   const groupRef = useRef();
+   useFrame((state, delta) => {
+      if (mouseOut) {
+         state.pointer.x = 0;
+         state.pointer.y = 0;
+      }
+      easing.dampE(
+         groupRef.current.rotation,
+         [0, state.pointer.x / 3, 0],
+         0.25,
+         delta
+      );
+   });
+   const handleMouseOut = () => {
+      setMouseOut(true);
+   };
+   const handleMouseOver = () => {
+      setMouseOut(false);
+   };
+   useEffect(() => {
+      window.addEventListener('mouseout', handleMouseOut);
+      window.addEventListener('mouseover', handleMouseOver);
+      return () => {
+         window.removeEventListener('mouseout', handleMouseOut);
+         window.removeEventListener('mouseover', handleMouseOver);
+      };
+   }, [mouseOut]);
+   return (
+      <group ref={groupRef}>
+         {children}
+      </group>
+   );
+}
