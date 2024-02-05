d91f4a538b520dbef806566c36ec0f5d08fe8d02
Atul chourasiya <82812953+atulchourasiya@users.noreply.github.com>
2024-02-05 23:04:28 +0530

    Fixed Skill Card Padding
    Signed-off-by: Atul chourasiya <82812953+atulchourasiya@users.noreply.github.com>

--- src/Component/Overlay/Skills/SkillCard.jsx
+++ src/Component/Overlay/Skills/SkillCard.jsx
@@ -13,8 +13,10 @@ const SkillCard = ({ title, image }) => {
       scale={1.1}
     >
       <Tooltip title={title}>
-        <div className='bg-white bg-opacity-90 flex  rounded  min-h-[4rem] min-w-[4rem] max-h-[5rem] max-w-[5rem] w-full h-full mx-2 md:w-[8rem] md:h-[8rem]  p-2'>
-          <img src={image} onLoad={handleImageLoad} className='opaque-image object-contain' alt="" />
+        <div className='bg-white bg-opacity-90 flex  rounded  min-h-[4rem] min-w-[4rem] max-h-[5rem] max-w-[5rem] w-full h-full mx-2 md:w-[8rem] md:h-[8rem] '>
+          <div className='p-2'>
+            <img src={image} onLoad={handleImageLoad} className='opaque-image object-contain' alt="" />
+          </div>
           {!isLoaded && <Skeleton variant="rectangular" className=' !hidden md:!block rounded-circle w-full md:!h-full' />}
         </div>
       </Tooltip>
